1ccaa476295bb60c9d78fa0a477df1315c947735
sanzida1635 <161277396+sanzida1635@users.noreply.github.com>
2024-02-27 20:31:37 +0600

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,39 @@
+var FirstName= "Hoora "
+var LastName= "Zannat"
+var FullName= FirstName+LastName
+console.log(FullName)
+Result = Hoora Zannat
+
+
+var a= 5
+var b= "60"
+console.log(a+Number(b))
+Result = 65
+
+
+
+var Num1=59
+var txt1="hello JS"
+console.log(Num1==txt1) //Result= False
+console.log(Num1===txt1) //Result= False
+console.log(Num1!==txt1)//Result= True
+console.log(Num1>=txt1)//Result= False
+console.log(Num1<=txt1)//Result= False
+
+
+
+var Num2 = 344
+var txtNum ="344"
+console.log(Num2==txtNum) //Result= True
+console.log(Num2===txtNum)//Result= False
+console.log(Num2!==txtNum)//Result= True
+console.log(Num2>txtNum) //Result= False
+console.log(Num2<txtNum) //Result= False
+console.log(Num2>=txtNum) //Result= True
+console.log(Num2<=txtNum) //Result= True
+
+
+var oddNum = 75;
+var evenNum = 90;
+var boolValue = oddNum !== evenNum;
+console.log(boolValue);
